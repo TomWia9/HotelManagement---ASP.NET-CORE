eca4e52bc539751ac62fb1b527ee73f100b7edc8
Tomasz Wiatrowski <tomaszwiatrowski9@gmail.com>
2020-10-13 09:22:26 +0200

    Add dbContext

---
+++ HotelManagement/Models/HotelManagementContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HotelManagement.Models
+{
+    public class HotelManagementContext: DbContext
+    {
+        public HotelManagementContext(DbContextOptions<HotelManagementContext> options) : base(options) 
+        {
+        }
+
+
+
+    }
+}
--- HotelManagement/Startup.cs
+++ HotelManagement/Startup.cs
@@ -2,9 +2,12 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using HotelManagement.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -12,10 +15,20 @@ namespace HotelManagement
 {
     public class Startup
     {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; set; }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<HotelManagementContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("HotelManagementConnection")));
+            
             services.AddControllers();
             services.AddSwaggerGen();
         }
