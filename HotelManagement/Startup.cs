2dec12bbc1fff9f13c5a74d5c162bdffb02d66f9
Tomasz Wiatrowski <tomaszwiatrowski9@gmail.com>
2020-10-13 09:10:57 +0200

    Add swagger

--- HotelManagement/Startup.cs
+++ HotelManagement/Startup.cs
@@ -16,6 +16,8 @@ namespace HotelManagement
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllers();
+            services.AddSwaggerGen();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -26,14 +28,19 @@ namespace HotelManagement
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "HotelManagement V1");
+                c.RoutePrefix = string.Empty;
+            });
+
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllers();
             });
         }
     }
