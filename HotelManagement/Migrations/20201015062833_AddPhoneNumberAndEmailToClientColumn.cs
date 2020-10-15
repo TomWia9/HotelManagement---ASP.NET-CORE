ea8d804c634e009329d58d7b1289bc3265e7880b
Tomasz Wiatrowski <tomaszwiatrowski9@gmail.com>
2020-10-15 08:32:44 +0200

    Add email and phone to client model

---
+++ HotelManagement/Migrations/20201015062833_AddPhoneNumberAndEmailToClientColumn.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace HotelManagement.Migrations
+{
+    public partial class AddPhoneNumberAndEmailToClientColumn : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Clients_Bookings_BookingId",
+                table: "Clients");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "BookingId",
+                table: "Clients",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Email",
+                table: "Clients",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "PhoneNumber",
+                table: "Clients",
+                nullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Clients_Bookings_BookingId",
+                table: "Clients",
+                column: "BookingId",
+                principalTable: "Bookings",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Clients_Bookings_BookingId",
+                table: "Clients");
+
+            migrationBuilder.DropColumn(
+                name: "Email",
+                table: "Clients");
+
+            migrationBuilder.DropColumn(
+                name: "PhoneNumber",
+                table: "Clients");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "BookingId",
+                table: "Clients",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int));
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Clients_Bookings_BookingId",
+                table: "Clients",
+                column: "BookingId",
+                principalTable: "Bookings",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
--- HotelManagement/Models/Client.cs
+++ HotelManagement/Models/Client.cs
@@ -12,6 +12,8 @@ namespace HotelManagement.Models
         public string LastName { get; set; }
         public Sex Sex { get; set; }
         public int Age { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
         public int AddressId { get; set; }
         public Address Address { get; set; }
         public int BookingId { get; set; }
