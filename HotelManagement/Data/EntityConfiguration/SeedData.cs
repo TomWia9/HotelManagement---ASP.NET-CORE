9cac5e3c77ede45509a2f96882f802b69be3bdb0
Tomasz Wiatrowski <tomaszwiatrowski9@gmail.com>
2020-11-10 17:03:35 +0100

    Apply Seeding data (default administrator if none exists)

---
+++ HotelManagement/Data/EntityConfiguration/SeedData.cs
@@ -0,0 +1,37 @@
+﻿using HotelManagement.Models;
+using HotelManagement.Shared;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HotelManagement.Data.EntityConfiguration
+{
+    public class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using var context = new DatabaseContext(
+                serviceProvider.GetRequiredService<
+                    DbContextOptions<DatabaseContext>>());
+
+            if (context.Administrators.Any())
+            {
+                return;   // DB has been seeded
+            }
+
+            context.Administrators.Add(
+                new Admin
+                {
+                    Email = "admin@admin",
+                    FirstName = "Admin",
+                    LastName = "Admin",
+                    Password = Hash.GetHash("admin")
+                }
+            );
+            context.SaveChanges();
+        }
+    }
+}
